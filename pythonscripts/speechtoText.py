f47b82d15d92c38a49fcacabe46ea8553ad265d8
Sohil <sadwani@umich.edu>
2019-09-21 11:09:58 -0400

    Finished speech to tex

--- pythonscripts/speechtoText.py
+++ pythonscripts/speechtoText.py
@@ -1,6 +1,10 @@
-from ibm_watson import SpeechToTextV1
-
-speech_to_text = SpeechToTextV1(
-    iam_apikey='{apikey}',
-    url='{url}'
-)
+from rev_ai import apiclient
+import time
+from apihelper import *
+# create your client
+client = apiclient.RevAiAPIClient(key)
+job = client.submit_job_local_file("./recording.m4a")
+jobs = client.get_list_of_jobs()
+time.sleep(35)
+transcript_text = client.get_transcript_text(job.id)
+print(transcript_text)
