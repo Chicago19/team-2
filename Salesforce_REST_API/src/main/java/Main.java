bb9dd5c53fbb6473fe503ac074928f4b29c50d69
Paul J Moore <pjmoore92@gmail.com>
2019-10-18 17:29:47 +0100

    update update

--- Salesforce_REST_API/src/main/java/Main.java
+++ Salesforce_REST_API/src/main/java/Main.java
@@ -26,8 +26,8 @@ public class Main {
     static final String PASSWORD = "password+secrettoken";
     static final String LOGINURL = "https://login.salesforce.com";
     static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
-    static final String CLIENTID = "";
-    static final String CLIENTSECRET = "";
+    static final String CLIENTID = "id";
+    static final String CLIENTSECRET = "secret";
     private static String REST_ENDPOINT = "/services/data";
     private static String API_VERSION = "/v32.0";
     private static String baseUri;
